b94b88ceb794c1b709ff88f6b03cf678e28c79b4
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-21 14:21:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.each(attendees) { |badge| puts badge}
-  room_assignments.each(attendees) { |room| puts room}
+  batch_badge_creator(attendees).each { |badge| puts badge}
+  room_assignments(attendees).each { |room| puts room}
 end
