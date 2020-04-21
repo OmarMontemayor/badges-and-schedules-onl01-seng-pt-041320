8eaa078ebea9571d8640d712a3606141d26901bc
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-21 14:06:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 
 def batch_badge_creator(attendees)
   badges = []
-  attendees.each { |badge| badges.push("Hello, my name is #{badge}.")
+  attendees.each { |badge| badges.push("Hello, my name is #{badge}.") }
   badges
 end
 
