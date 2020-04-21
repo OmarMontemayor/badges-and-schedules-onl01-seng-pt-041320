f62ffc6bfe26f845326a2a932ad84c490d418b29
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-21 13:59:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each { |badge| badges.push("Hello, my name is #{badge}.")
+  attendees
+end
+
+def assign_rooms(array)
+  array.each_with_index {
